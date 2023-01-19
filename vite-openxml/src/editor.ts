199f728c8240a749621d28d0ecc852109cc32c2e
r-southworth <rachael.southworth@datagrove.com>
2023-01-19 12:04:10 -0500

    try to configure codemirror

--- vite-openxml/src/editor.ts
+++ vite-openxml/src/editor.ts
@@ -1,13 +1,37 @@
-import {EditorState} from "@codemirror/state"
-import {EditorView, keymap} from "@codemirror/view"
-import {defaultKeymap} from "@codemirror/commands"
+// import {EditorState} from "@codemirror/state"
+// import {EditorView, keymap} from "@codemirror/view"
+// import {defaultKeymap} from "@codemirror/commands"
+// import { basicSetup } from "@codemirror/basic-setup"
+// import { markdown } from "@codemirror/lang-markdown"
+// import { oneDarkTheme } from "@codemirror/theme-one-dark"
 
-let startState = EditorState.create({
-  doc: "Hello World",
-  extensions: [keymap.of(defaultKeymap)]
-})
+import {basicSetup, EditorView} from "codemirror"
+import {markdown} from "@codemirror/lang-markdown"
+import {languages} from "@codemirror/language-data"
 
-let view = new EditorView({
-  state: startState,
-  parent: document.getElementById('codemirror')!
+
+// let startState = EditorState.create({
+//   doc: "Hello World",
+//   extensions: [
+//     basicSetup,
+//     // oneDarkTheme,
+//     // markdown(),
+//   ],
+// })
+
+// let view = new EditorView({
+//   doc: "hello markdown",
+//   extensions: [
+
+//   ],
+//   parent: document.getElementById('codemirror')!
+// })
+
+let editor = new EditorView({
+  doc: "Hello\n\n```javascript\nlet x = 'y'\n```",
+  extensions: [
+    basicSetup,
+    markdown({codeLanguages: languages})
+  ],
+  parent: document.getElementById('editor')!
 })
--- vite-openxml/tailwind.config.cjs
+++ vite-openxml/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./*.html"
+  ],
   theme: {
     container:{
       padding:{
